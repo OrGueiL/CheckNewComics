4e9bef4d400a334fdfb14c36a084187011e5e4b6
OrGueiL <31163777+OrGueiL@users.noreply.github.com>
2018-02-21 23:05:08 +0100

    small fixes

--- newComicsBot.py
+++ newComicsBot.py
@@ -1,12 +1,20 @@
 import feedparser
 from slackclient import SlackClient
 
+# Slack channel where you send the message
+channame = ""
 
 class SlackBot():
 
 	def __init__(self):
-		# connect to Slack
+		##############
+		### CONFIG ###
+		##############
+
 		self.slack_token  = ""
+		self.botname = ""
+
+		# connect to Slack
 		self.slack_client = SlackClient(self.slack_token)
 
 
@@ -16,7 +24,7 @@ class SlackBot():
 		send = self.slack_client.api_call("chat.postMessage",
 			channel=to, 
 			text=message,
-			username="",
+			username= self.botname,
 			as_user="true"
 			)
 		return send
@@ -27,7 +35,7 @@ class RSS():
 
 	def __init__(self):
 		self.url = 'http://feeds.feedburner.com/PlaneteBdLight-ChroniquesComics?format=xml'
-		self.datefile = 'lastupdate.txt'
+		self.filename = "lastupdate.txt"		
 
 	def checkRSSFeed(self):
 		feed = feedparser.parse(self.url)
@@ -39,10 +47,10 @@ class RSS():
 		# create the file if it doesn't exist
 
 		try:
-			datefile = open(self.datefile, "r")
+			datefile = open(self.filename, "r")
 			lastupdate = datefile.read() 
 		except IOError:
-			datefile = open(self.datefile, "w")
+			datefile = open(self.filename, "w")
 			lastupdate = date
 			datefile.write(lastupdate)
 
@@ -54,7 +62,7 @@ class RSS():
 	def updateDate(self, date):
 		# update the last update date in a file
 
-		datefile = open(self.datefile, "w")
+		datefile = open(self.filename, "w")
 		datefile.write(date)
 		datefile.close()
 
@@ -62,6 +70,7 @@ class RSS():
 	def checkIfNew(self, date, infos):
 		# check if there is a new post in the RSS feed
 
+		# parse to a real date format
 		post_update = date[0:10] + ' ' + date[11:-1]
 		lastupdate = self.checkLastUpdate(post_update)
 
@@ -78,4 +87,4 @@ feed = flux.checkRSSFeed()
 for post in reversed(feed['entries']):
 	infos = '*' +post['title'] + '* ' + post['id']
 	if flux.checkIfNew(post['updated'], infos):
-		bot.send_message_to_slack("",infos)
+		bot.send_message_to_slack(channame,infos)
